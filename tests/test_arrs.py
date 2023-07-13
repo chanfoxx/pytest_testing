e0bfacac2e142747430bbc47acf0e1586cb7da58
Oh Olya <chanfoxx@yandex.ru>
2023-07-13 18:31:17 +0300

    Fixes test_get, test_get__empty_array

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,9 +1,15 @@
+import pytest
+
 from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([1, 2, 3], 1, "test") == 2
+
+
+def test_get__empty_array():
+    with pytest.raises(IndexError):
+        arrs.get([], 0)
 
 
 def test_slice():
