2501bcc96313218f7d40d070f9aa31367490f921
Oh Olya <chanfoxx@yandex.ru>
2023-07-14 17:51:00 +0300

    Fixes tests, adding parametrize and fixture, coverage 100%

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -1,9 +1,22 @@
+import pytest
+
 from utils import dicts
 
 
-def test_get_val():
-    assert dicts.get_val({"mitsubishi": "japan", "audi": "germany",
-                          "kia": "south korea", "toyota": "japan"}, "audi") == "germany"
-    assert dicts.get_val({"mitsubishi": "japan", "audi": "germany",
-                          "kia": "south korea", "toyota": "japan"}, "volvo") == "git"
-    assert dicts.get_val(("mitsubishi", "audi", "kia", "toyota"), "audi") == "git"
+@pytest.mark.parametrize('collection, key, expected', [
+    (("mitsubishi", "audi", "kia", "toyota"), "audi", "unknown value"),
+    ({}, "audi", "unknown value"),
+    (None, "audi", "unknown value"),
+    (123, "audi", "unknown value")
+])
+
+def test_get_val(collection, key, expected):
+    assert dicts.get_val(collection, key) == expected
+
+def test_get_val__fixture(coll_fixture):
+    assert dicts.get_val(coll_fixture, "audi") == "germany"
+    assert dicts.get_val(coll_fixture, "volvo") == "unknown value"
+
+def test_get_val__missing_key(coll_fixture):
+    with pytest.raises(TypeError):
+        dicts.get_val(coll_fixture)
